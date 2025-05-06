687fcbee5879601a2f8088ff55791b6a8f84e158
Itay Leon <itayleon81@walla.com>
2025-05-06 22:07:51 +0300

    Add initial Python script with variable assignments and print statements

---
+++ 06-05-2025/01-varables/src/app.py
@@ -0,0 +1,34 @@
+print("Hello, World!") #printing to console
+# This is a simple Python script that prints "Hello, World!" to the console.
+
+print("first string", "second string") # printing multiple strings
+
+# variable assignments - strings
+name = "Shir Shlomy"
+print(name)
+
+print("Hello World!") 
+print("Hello \nWorld!") # printing with a new line with \n
+print("Hello \"World!\"") # printing with a backslash with \"
+print("Hello \tWorld!") # printing with a tab with \t
+print("Hello \\World!") # printing with a backslash with \\
+
+# variable assignments
+first_name = "Shir"
+print("your name is:", first_name)
+first_name = "Itay"
+print("your name is:", first_name)
+
+last_name = "Leon"
+full_name = first_name + " " +  last_name
+print("your name is:", full_name)
+char1 = "a"
+char2 = "b"
+sum_chars = char1 + " " + char2
+print("sum of chars is:", sum_chars)
+
+
+
+
+
+
