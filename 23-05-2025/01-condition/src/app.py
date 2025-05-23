ef0ec6c2afc7041589a238db055d28892691971c
Itay Leon <itayleon81@walla.com>
2025-05-23 23:28:07 +0300

    Add initial implementation of app.py with condition examples and user input handling; create package.json and package-lock.json for dependencies

---
+++ 23-05-2025/01-condition/src/app.py
@@ -0,0 +1,52 @@
+# Condition
+number = 10
+if (number > 0):
+    print("Positive number")
+
+#Syntax 2: if (condition):(code) else:(code)   
+# Example 1
+number = -10
+if (number > 0):
+    print("Positive number")
+else:
+    print("Negative number")    
+
+
+#syntax 3: if (condition):(code) elif (condition):(code) else:(code)
+# elif: else if   
+# Example 2
+number = 0
+if (number > 0):
+    if (number > 10):
+        print("Greater than 10")
+    elif (number < 10):
+        print("Less than 10")
+    else:
+        print("Equal to 10")        
+elif (number == 0):
+    print("Zero")
+else:
+    print("Negative number")
+    
+#ex1
+char = input("Enter a character: ")
+if((char >= 'A' and char <= 'Z') or (char >= 'a' and char <= 'z')):
+    print("character")
+elif (char >= '0' and char <= '9'):
+    print("digit")
+else:
+    print("special character")    
+
+#ex2
+score = int(input("Enter your score: "))
+if(score >= 90):
+    print("Excellent")
+elif((score >= 80) and (score < 90)):
+    print("Very Good")
+elif((score >= 60) and (score < 80)):
+    print("Passed")
+    if((score >= 60) and (score < 70)):
+        print("You should work hard")
+else:
+    print("Failed")
+             
