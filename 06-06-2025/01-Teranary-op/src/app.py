203227d5fdc9f08c4b8732326588b0829b1e5e8a
Itay Leon <itayleon81@walla.com>
2025-06-06 21:38:45 +0300

    Add initial implementation of package.json and package-lock.json; create app.py with ternary operator logic

---
+++ 06-06-2025/01-Teranary-op/src/app.py
@@ -0,0 +1,12 @@
+#Ternary Operators:
+#Syntax: variable = value_if_true if condition else value_if_false
+
+num1 = 10
+num2 = 20
+max_num = 0
+max_num = num1 if num1 > num2 else num2
+
+if num1 > num2:
+    max_num = num1
+else:
+    max_num = num2
